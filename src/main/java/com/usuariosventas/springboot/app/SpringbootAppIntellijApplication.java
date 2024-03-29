8f2001ad7381850f8231e5bce031b1cf4e16ec80
chris <chriskhun123@gmail.com>
2024-03-29 01:33:53 -0300

    primer commit.

---
+++ src/main/java/com/usuariosventas/springboot/app/SpringbootAppIntellijApplication.java
@@ -0,0 +1,13 @@
+package com.usuariosventas.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppIntellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppIntellijApplication.class, args);
+	}
+
+}
